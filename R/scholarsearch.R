550b0d925915c69fbdef3b2d73bec7804509e64f
Robin Lovelace <rob00x@gmail.com>
2019-09-22 12:05:45 +0100

    Add as_yhi param

--- R/scholarsearch.R
+++ R/scholarsearch.R
@@ -1,20 +1,35 @@
 #' Search google scholar
 #'
 #' @param query Search query
-#' @param as_ylo Start year
+#' @param as_ylo Lowest year
+#' @param as_yhi Highest year
 #' @param source Journal
+#' @param open_browser Open the browser?
 #' @param u Base URL
 #'
 #' @examples
+#' scholarsearch(query = "bikeshare data analysis")
 #' scholarsearch(query = "bikeshare data analysis", source = "transportation research part A")
 #' scholarsearch(query = "bikeshare data analysis", source = "transportation research part A", as_ylo = 2018)
+#' scholarsearch(query = "bikeshare data analysis", source = "transportation research part A", as_yhi = 2012)
 #' @export
-scholarsearch = function(query = NULL, as_ylo = NULL, source = NULL, u = "https://scholar.google.co.uk/scholar") {
-  query = gsub(pattern = " ", replacement = "+", x = query)
-  query = paste0(query, '+source:"', source, '"')
-  q = list(q = query, as_ylo = as_ylo)
+scholarsearch = function(query = NULL,
+                         as_ylo = NULL,
+                         as_yhi = NULL,
+                         source = NULL,
+                         open_browser = TRUE,
+                         u = "https://scholar.google.co.uk/scholar") {
+  query = gsub(pattern = " ",
+               replacement = "+",
+               x = query)
+  if (!is.null(source)) {
+    query = paste0(query, '+source:"', source, '"')
+  }
+  q = list(q = query,
+           as_ylo = as_ylo,
+           as_yhi = as_yhi)
   search_url = httr::modify_url(url = u, query = q)
-  browseURL(search_url)
+  if(open_browser) browseURL(search_url)
   search_url
 }
 # https://scholar.google.co.uk/scholar?q=bikeshare
