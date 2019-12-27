20b1b945dde3db721ffb58b54073424c2a2f3fe7
Robin Lovelace <rob00x@gmail.com>
2019-12-27 09:49:21 +0000

    Add working package

---
+++ R/get_citations.R
@@ -0,0 +1,35 @@
+#' Scrape title, author and number of citations from google scholar url
+#'
+#' @param u url
+#' @export
+#' @examples
+#' query = "open source software"
+#' u = scholarurl(query)
+#' res = scholar_get_citations(u)
+#' res
+scholar_get_citations = function(u) {
+  html = xml2::read_html(u)
+  titles = rvest::html_nodes(html, ".gs_rt a")
+  titles_text = rvest::html_text(titles)
+  author_source = rvest::html_nodes(html, ".gs_a") # author, source
+  as_text = rvest::html_text(author_source)
+  authors = stringr::str_extract(string = as_text, pattern = "[^-]+")
+  year = stringr::str_extract(string = as_text, pattern = "[0-9][0-9][0-9][0-9]")
+  first_author = stringr::str_extract(authors, "[^,]+")
+  first_author_sirname = stringr::str_replace_all(first_author, "[A-Z]+ ", "")
+  n = rvest::html_nodes(html, ".gs_ri .gs_fl") # author, source
+  n_text = rvest::html_text(n, ".gs_ri .gs_fl") # author, source
+  n_citations = stringr::str_extract(string = n_text, pattern = "(?<=[Cited by ])\\d+")
+  n_results = rvest::html_nodes(html, ".gs_ab_mdw")
+  n_results_txt = rvest::html_text(n_results)[2]
+  n_results = stringr::str_extract(n_results_txt, "[0-9|,]+")
+  n_results = gsub(pattern = ",", replacement = "", n_results)
+  tibble::tibble(
+    first_author_sirname,
+    year = as.integer(year),
+    titles_text,
+    n_citations = as.integer(n_citations),
+    n_results = as.integer(n_results),
+    url = u
+  )
+}
--- R/scholarsearch.R
+++ R/scholarsearch.R
@@ -12,6 +12,8 @@
 #' scholarsearch(query = "bikeshare data analysis", source = "transportation research part A")
 #' scholarsearch(query = "bikeshare data analysis", source = "transportation research part A", as_ylo = 2018)
 #' scholarsearch(query = "bikeshare data analysis", source = "transportation research part A", as_yhi = 2012)
+#' scholarsearch(query = '"open+source"+"transport+planning"', as_yhi = 2012) # note google's citation format
+#' scholarsearch(query = '"open+source"+"transport+planning"', as_yhi = 2012) # note google's citation format
 #' @export
 scholarsearch = function(query = NULL,
                          as_ylo = NULL,
@@ -32,5 +34,38 @@ scholarsearch = function(query = NULL,
   if(open_browser) browseURL(search_url)
   search_url
 }
-# https://scholar.google.co.uk/scholar?q=bikeshare
-# https://scholar.google.co.uk/scholar?as_ylo=2015&q=bikeshare+data+analysis+source:"Transportation+Research+Part+A"
+#' Get url of google scholar search
+#' @inheritParams scholarsearch
+#' @aliases scholarsearch
+#' @export
+#' @examples
+#' scholarurl("test")
+scholarurl = function(query) {
+  scholarsearch(query, open_browser = FALSE)
+}
+#' Search google scholar
+#'
+#' @param query Search query
+#' @param as_ylo Lowest year
+#' @param as_yhi Highest year
+#' @param source Journal
+#' @param open_browser Open the browser?
+#' @param u Base URL
+#'
+#' @examples
+#' scholar_get(query = '"open+source"+"transport+planning"')
+#' @export
+scholar_get = function(query = NULL,
+                         as_ylo = NULL,
+                         as_yhi = NULL,
+                         source = NULL,
+                         open_browser = FALSE,
+                         u = "https://scholar.google.co.uk/scholar") {
+  u = scholarsearch(query,
+                    as_ylo,
+                    as_yhi,
+                    source,
+                    open_browser,
+                    u = "https://scholar.google.co.uk/scholar")
+  scholar_get_citations(u)
+}
